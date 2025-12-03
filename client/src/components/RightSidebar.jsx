758b745d1945d380f6e2882e8b6744afd1b7e060
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-12-03 23:33:02 +0530

    fee update

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import assets, { imagesDummyData } from '../assets/assets'
 
-const RightSidebar = () => {
-  return (
-    <div>
-      <h1>RightSlidebar</h1>
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
+    <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden": ""}`}>
+      <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-[1/1] rounded-full' />
+        <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
+          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {selectedUser.fullName}</h1>
+          <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
+      <hr className='border-[#ffffff50] my-4'/>
+      <div className='px-5 text-xs'>
+        <p>Media</p>
+        <div className='mt-2 max-h-[200px] overflow-y-sc'>
+          {imagesDummyData}
+        </div>
+      </div>
     </div>
   )
 }
