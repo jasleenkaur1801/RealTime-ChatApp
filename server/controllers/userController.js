9c64dc7693e32ee80450a8712267b4aabc3437ef
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-12-05 22:43:51 +0530

    bee added

---
+++ server/controllers/userController.js
@@ -0,0 +1,13 @@
+
+
+//signup a new user
+export const signup = async (req,res) => {
+    const {fullName, email, password, bio} = req.body;
+    try{
+        if(!fullName || !email || !password || !bio){
+            
+        }
+    }catch(error){
+
+    }
+}
