5bd581d02de810af5582ac7a1bcaecc021a92717
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-12-07 22:24:36 +0530

    authjs

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,8 +8,16 @@ export const protectRoute = async (res,res,next)=>{
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
         const user = await User.findById(decoded.userId).select("-password");
         
-        if(!user) return res.json({success: false, message: "User not found"})
+        if(!user) return res.json({success: false, message: "User not found"});
+        req.user = user;
+        next();
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({success: false,message: error.message});
     }
+}
+
+//controller to check if user is authenticated
+export const checkAuth = (res,res)=>{
+    res.json({success: true,user: req.user});
 }
