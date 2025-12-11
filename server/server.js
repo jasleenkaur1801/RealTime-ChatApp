710c544f483e5d6dd6c9fc8801aa7b21f46a4ea3
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-12-11 23:27:40 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -5,10 +5,35 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
-
+import {Server} from "socket.io";
 //create express app and http server
 const app = express();
 const server = http.createServer(app)
+
+//initialize socket.io server
+export const io = new Server(server,{
+    cors: {origin: "*"}
+})
+
+//store online users
+export const userSocketMap = {};
+
+//socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId)userSocketMap[userId] = socket.id;
+
+    //Emit online users to all connected clients
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    })
+})
 
 //middlewares
 app.use(express.json({limit: "4mb"}));
