d27afa2f404475bc2ceb12783624ecfef547fab7
jasleenkaur1801 <jasskaur4m@gmail.com>
2025-12-12 23:32:59 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,13 @@ import {Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import {Toaster} from 'react-hot-toast'
+
 const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')]
   bg-contain bg-no-repeat bg-center bg-[#060606]">
+    <Toaster/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
